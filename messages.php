0236c5abce684bebdcc1ab558a33c2b912999483
deadlightreal <richardfabianmain@gmail.com>
2023-10-15 21:33:44 +0200

    added displaying users messages comming soon

--- messages.php
+++ messages.php
@@ -4,12 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messages</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        include "database.php";
-        session_start();
+    <div class="usersMessages">
+        <?php
+            include "database.php";
+            session_start();
 
+            $selectUsers = "SELECT * FROM `users`";
+            $displayUsers = mysqli_query($db, $selectUsers);
+
+            while ($resultsUsers = mysqli_fetch_array($displayUsers)) {
+                echo '<form action="messagesDisplay" method="POST" class="userForm">';
+                echo '<input type="submit" class="user" value="' . $resultsUsers["username"] . '">';
+                echo '</form>';
+            }
+        ?>
+
+    </div>
+    <?php
         $username = $_SESSION["username"];
 
         $select = "SELECT * FROM `messages` WHERE `sender` LIKE '$username'";
